90d7a0e38bc1bbe865a18bd41ad523b02a53e58a
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-03-25 15:29:20 +0100

    Bug in tests

--- tests/testthat/test-estimate_pcf_fast.R
+++ tests/testthat/test-estimate_pcf_fast.R
@@ -1,7 +1,10 @@
 testthat::context("test-estimate_pcf_fast")
 
-pcf <- shar::estimate_pcf_fast(pattern = shar::species_b)
-
 testthat::test_that("estimate_pcf returns spatstat.fv object", {
-  testthat::expect_is(pcf, "fv")
+
+  pattern <- spatstat::runifpoint(n = 100)
+
+  pcf_est <- estimate_pcf_fast(pattern)
+
+  testthat::expect_is(pcf_est, "fv")
 })
